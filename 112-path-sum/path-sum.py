ccdb94a3d5ebfdae67670d3dfcce719468bbb304
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-07-14 13:55:57 -0400

    Time: 3 ms (28.90%) | Memory: 20.2 MB (20.06%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -1,31 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: Optional[TreeNode]
-        :type targetSum: int
-        :rtype: bool
-        """
-        # no tree or all nodes visited: return false
-        # solution: recursively check left/right and subtract from target
-          # if targetSum is 0 and node is leaf(no l/r): return True
-
-        # edge case
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
-            return False 
-
-        # check if leaf and target reached
-        targetSum = targetSum - root.val 
+            return False
+        
+        targetSum -= root.val
         if targetSum == 0 and (not root.right and not root.left):
             return True
         
-        # dfs 
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
-        
-        
-        
