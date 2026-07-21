9619f4bd1dcfdc8f305ba33be197361c1ebab735
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-07-21 15:30:02 -0400

    Time: 130 ms (52.58%) | Memory: 31.9 MB (69.58%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        # if not root then return 0, else res = 1
+        # dfs left/right
+        # maintain a max value, if curr node is greater= then res += 1
+        if not root:
+            return 0
+        
+        def dfs(node, max_val):
+            res = 0
+            # base case
+            if not node:
+                # return unwinds one call from stack
+                return 0 
+
+            if node.val >= max_val:
+                res += 1
+                max_val = node.val
+
+            res += dfs(node.left, max_val) + dfs(node.right, max_val) 
+            return res
+
+        return dfs(root, root.val)
+            
