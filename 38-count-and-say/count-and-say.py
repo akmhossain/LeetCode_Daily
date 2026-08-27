aea63ad5f4a87859017d3addef0f50dbf8b4faa7
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-08-27 15:06:13 -0400

    Time: 7 ms (73.81%) | Memory: 19.2 MB (71.14%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        # base case: 1
+        
+        def getRLE(s):
+            count = 0
+            curr_num = s[0]
+            res = ""
+
+            # s = 11
+            for i in range(len(s)):
+                if s[i] == curr_num:
+                    count += 1
+                else:
+                    res += str(count)
+                    res += curr_num
+                    count, curr_num = 1, s[i]
+            
+            if count > 0:
+                res += str(count)
+                res += curr_num
+
+            return res
+
+        dp = ["1"] # 11
+        for i in range(n-1):
+            RLE = getRLE(dp[i])
+            dp.append(RLE)
+        
+        return dp[-1]
+
+            
