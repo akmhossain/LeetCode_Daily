2bf51da203a6375d4e91fdbaa7dd49c476e83672
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-08-27 22:49:29 -0400

    Time: 4 ms (30.76%) | Memory: 19.3 MB (80.01%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,12 +3,11 @@ class Solution:
         # if len == 2, return min(cost[0], cost[1])
         # if len == 3, return min(cost[0] + cost[2], cost[1])
         # if len == 4, return min
-        # start backwards with dp array, cost at the end is 0
 
-        dp = [0] * (len(cost) + 2) # dp[i] is the min to climb to top from i
+        dp = [0] * (len(cost) + 1) # dp[i] is the min to climb to top from i
 
-        for i in range(len(cost)-1, -1, -1):
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+        for i in range(2,len(cost)+1):
+            dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2])
         
-        return min(dp[0], dp[1])
+        return dp[-1]
 
