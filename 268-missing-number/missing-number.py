fcbc44db88ddf4f2a945a216268b3e5d42860d4a
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-09-04 23:57:12 -0400

    Time: 1394 ms (11.11%) | Memory: 20.4 MB (75.49%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        seen = [0] * (len(nums) + 1) # each index represents num
-        for i in nums:
-            seen[i] = 1
+        # seen = [0] * (len(nums) + 1) # each index represents num
+        # for i in nums:
+        #     seen[i] = 1
         
-        for i in range(len(seen)):
-            if seen[i] == 0:
-                return i
+        # for i in range(len(seen)):
+        #     if seen[i] == 0:
+        #         return i
+
+        missing = 0
+        while missing < len(nums):
+            if missing in nums:
+                missing += 1
+            else:
+                return missing
+        
+        return missing 
