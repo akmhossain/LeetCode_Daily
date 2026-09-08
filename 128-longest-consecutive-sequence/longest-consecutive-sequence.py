f45598564af577c90a151d7d4233370759fa44d7
AKM Hossain <139937171+akmhossain@users.noreply.github.com>
2026-09-08 12:55:51 -0400

    Time: 43 ms (85.10%) | Memory: 36.9 MB (12.33%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -9,10 +9,9 @@ class Solution:
         for i in s:
             if i - 1 not in s:
                 curr_max = 0
-                curr_num = i
-                while curr_num in s:
+                while i in s:
                     curr_max += 1
-                    curr_num += 1
+                    i += 1
                 res = max(res, curr_max)
         
         return res
